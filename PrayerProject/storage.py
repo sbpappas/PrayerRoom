a11064fe6e36ae24781d5132d27558696b888248
Samuel Pappas <spappas@trinity.edu>
2025-11-11 16:59:08 -0600

    additions

--- PrayerProject/storage.py
+++ PrayerProject/storage.py
@@ -19,7 +19,10 @@ def load_prayers() -> List[PrayerRequest]: # retrieve the data file
 def save_prayers(prayers: List[PrayerRequest]) -> None:
     DATA_FILE.write_text(json.dumps([p.to_dict() for p in prayers], indent=2), encoding="utf-8")
 
-def next_id(prayers: List[PrayerRequest]) -> int: 
+def next_prayer_id(prayers: List[PrayerRequest]) -> int: 
     return max((p.id for p in prayers), default=0) + 1
     # iterates over prayers and gets id of each prayer object
     # finds the next available id by getting MAX and adding one.
+
+def next_user_id(users: List[str]) -> int:
+    return max((u.id for u in users), default = 0) + 1
